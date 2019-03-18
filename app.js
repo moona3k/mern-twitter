ce8ffaf48ca0dd6d70422c2850ee92182d14a531
Daniel Moon <moona3k@gmail.com>
2019-03-17 23:01:27 -0700

    initialize app.js and set up express & connect to mongoDB

--- app.js
+++ app.js
@@ -6,9 +6,18 @@ const port = process.env.Port || 5000;
 // heroku requires us to run server on 'process.env.PORT'
 // localhost:5000
 
-app.get("/", (req, res) => res.send("hello world!"));
+app.get("/", (req, res) => res.send("yes testing?"));
 // make GET request to "/" url
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
 // tell express to start a socket and listen for connections on the path
+
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI; // imports the mongoDB user string from /config/keys.js
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("connected to mongooseDB succesfully"))
+    .catch(err => console.log(err));
+
 
